5d55b6289eb3eeecd6da6a5715148517bab19326
ristoluik <risto.luik@khk.ee>
2017-12-14 13:51:11 +0200

    Väljastatud värviline tekst

--- varv.php
+++ varv.php
@@ -5,6 +5,15 @@
  * Date: 14.12.2017
  * Time: 13:35
  */
-for ($kord = 1; $kord <= 5; $kord++){
-    echo 'Värviline tekst<br />';
+for ($rida = 1; $rida <= 5; $rida++){
+    $varv = '#';
+    for($kord = 1; $kord <= 6; $kord++){
+        $juhuarv = rand(0, 15);
+        $symbol = dechex($juhuarv);
+        $varv = $varv.$symbol;
+    }
+    echo  '<font color="'.$varv.'">"'.
+        'Värviline tekst'.
+        '</font>'.
+        '<br />';
 }
